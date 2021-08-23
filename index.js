6ea10dcffbc5c7dc64735c11b6ca708a069cc6e8
Krish Garg <krishgarg6306@gmail.com>
2021-08-24 04:53:29 +0530

    -2

--- index.js
+++ index.js
@@ -83,12 +83,7 @@ app.post('/api/shorten', async (req, res) => {
                 return;
             } else {
                 console.log(__dirname);
-                console.log(fs.readFile('./views'), (err, files) => {
-                    if (err) {console.error(err)}
-                    else {
-                        console.log(files);
-                    }
-                })
+                console.log(fs.readFileSync('./views'))
                 res.render('shortMade.ejs', {shortURL: obj.shortURL});
             }
         })
